cef6f27a627974ec78ff32720fbc64959f0df28e
Isaac Lim <isaaclimdc@gmail.com>
2014-04-26 07:57:58 -0400

    Added bus controller stubs

---
+++ src/modules/CMBusCtrlr.cpp
@@ -0,0 +1,12 @@
+/* CACHEMULATOR
+ * Yuyang Guo (yuyangg) and Isaac Lim (idl)
+ */
+
+#include "CMBusCtrlr.h"
+#include "debug.h"
+
+CMBusCtrlr::CMBusCtrlr() {
+}
+
+CMBusCtrlr::~CMBusCtrlr() {
+}
---
+++ src/modules/CMBusCtrlr.h
@@ -0,0 +1,13 @@
+/* CACHEMULATOR
+ * Yuyang Guo (yuyangg) and Isaac Lim (idl)
+ */
+
+#pragma once
+
+class CMBusCtrlr {
+  private:
+
+  public:
+    CMBusCtrlr();
+    ~CMBusCtrlr();
+};
