96512b6b8a68490e64f861b2802afaf6911bb386
Isaac Lim <isaaclimdc@gmail.com>
2014-05-08 16:35:37 -0400

    Making pinatrace better: reading in user funcs from file

--- pin/source/tools/ManualExamples/pinatrace.cpp
+++ pin/source/tools/ManualExamples/pinatrace.cpp
@@ -1,9 +1,13 @@
 #include <stdio.h>
-#include <cstring>
 #include "pin.H"
+#include <fstream>
+#include <vector>
+#include <cstring>
+#include <string>
 
 FILE *trace;
 PIN_LOCK lock;
+std::vector<std::string> userFuncs;
 
 // Print a memory read record
 VOID RecordMemRead(VOID *ip, VOID *addr, THREADID tid) {
@@ -19,20 +23,27 @@ VOID RecordMemWrite(VOID *ip, VOID *addr, THREADID tid) {
   PIN_ReleaseLock(&lock);
 }
 
-
 VOID Routine(RTN rtn, VOID *v) {
-  std::string name = RTN_Name(rtn);
+  std::string name = PIN_UndecorateSymbolName(RTN_Name(rtn).c_str(),
+                       UNDECORATION_NAME_ONLY);
 
-  if (strcmp(name.c_str(), "meowmeow") == 0) {
+  std::vector<std::string>::iterator it;
+  for (it = userFuncs.begin(); it != userFuncs.end(); ++it) {
+    std::string userFunc = *it;
+    if (name.compare(userFunc) != 0) continue;
+
     RTN_Open(rtn);
+
     // For each instruction of the routine
     for (INS ins = RTN_InsHead(rtn); INS_Valid(ins); ins = INS_Next(ins)) {
       UINT32 memOperands = INS_MemoryOperandCount(ins);
 
       // Iterate over each memory operand of the instruction.
       for (UINT32 memOp = 0; memOp < memOperands; memOp++) {
+        if (INS_IsStackRead(ins) || INS_IsStackWrite(ins))
+          break;
+
         if (INS_MemoryOperandIsRead(ins, memOp)) {
-          if (INS_IsStackRead(ins)) continue;
           INS_InsertPredicatedCall(
             ins, IPOINT_BEFORE, (AFUNPTR)RecordMemRead,
             IARG_INST_PTR,
@@ -40,7 +51,6 @@ VOID Routine(RTN rtn, VOID *v) {
             IARG_END);
         }
         if (INS_MemoryOperandIsWritten(ins, memOp)) {
-          if (INS_IsStackWrite(ins)) continue;
           INS_InsertPredicatedCall(
             ins, IPOINT_BEFORE, (AFUNPTR)RecordMemWrite,
             IARG_INST_PTR,
@@ -59,8 +69,24 @@ VOID Fini(INT32 code, VOID *v) {
 }
 
 int main(int argc, char *argv[]) {
-  trace = fopen("pinatrace.out", "w");
+  std::ifstream configFile;
+  configFile.open("userfuncs.in");
 
+  std::string s;
+  while (std::getline(configFile, s)) {
+    userFuncs.push_back(s);
+  }
+  configFile.close();
+
+  // std::vector<std::string>::iterator it;
+  // for (it = userFuncs.begin(); it != userFuncs.end(); ++it) {
+  //   std::string str = *it;
+  //   printf("func: %s\n", str.c_str());
+  // }
+
+  trace = fopen("user.trace", "w");
+
+  PIN_Init(argc, argv);
   PIN_InitSymbols();
 
   RTN_AddInstrumentFunction(Routine, 0);
