28f3bd7746f8c5d31604ab4f38f534e2fccc3bd8
Isaac Lim <isaaclimdc@gmail.com>
2014-05-08 22:45:08 -0400

    Shell script plots 3 graphs

--- src/cachemulator.sh
+++ src/cachemulator.sh
@@ -3,23 +3,47 @@
 # CACHEMULATOR
 # Yuyang Guo (yuyangg) and Isaac Lim (idl)
 
-# Note: Run this from "cachemulator/src"
+function cleanTmpFiles {
+  declare -a UGLYFILES=("*.out" "*.report" "*.tmp")
+  for UGLYFILE in "${UGLYFILES[@]}"
+  do
+    if ls $UGLYFILE &> /dev/null; then
+      rm $UGLYFILE
+    fi
+  done
+}
+
+####### Note: Run this from "cachemulator/src" #######
 
 # Make BFS code
 cd programs/bfs
 make
+cd ../..
 
 # Generate Pin trace
-cd ../../../pin
+cd ../pin
 ./pin -t pinatrace.so -- ../src/programs/bfs/bfs ../src/programs/bfs/tiny.graph
 cp user.trace ../src/traces/bfs.trace
-
-# Run cache
 cd ../src
-rm *.out *.report *.tmp
-make
-./cache -t traces/bfs.trace -p MSI
 
-# Plot graphs
-cd scripts
-./showBusTraffic.py $1
+# Build cache
+cleanTmpFiles
+make clean
+make
+
+# Run cache and plot graph for each protocol
+declare -a PROTOCOLS=("MSI" "MESI" "MESIF")
+for PROTOCOL in "${PROTOCOLS[@]}"
+do
+  # Run cache
+  ./cache -t traces/bfs.trace -p $PROTOCOL
+
+  # Plot graphs
+  cd scripts
+  ./showBusTraffic.py $PROTOCOL
+
+  cd ..
+done
+
+# Clean
+cleanTmpFiles
