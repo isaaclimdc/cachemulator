bdf776d46cefd94ee4ecc8d3e3c11fcef4224451
Yuyang Guo <yuyangg@andrew.cmu.edu>
2014-05-08 22:45:37 -0400

    better looking busTraffic chart

--- src/scripts/showBusTraffic.py
+++ src/scripts/showBusTraffic.py
@@ -14,14 +14,14 @@ stats = {
   'busUpg': [0],
 }
 
-
+groupSize = 500
 def main():
   outfile = sys.argv[1]
   with open(TRAFFIC_FILE) as f:
-    counter = 500
+    counter = groupSize
     for char in f.read():
       if counter <= 0:
-        counter = 500
+        counter = groupSize
         for stat in stats:
           stats[stat].append(0)
 
@@ -32,22 +32,26 @@ def main():
       elif char == 'W':
         stats['busWr'][-1] += 1
       elif char == 'U':
-        stats['busUpg'][-1] += 1
+        stats['busWr'][-1] += 1
 
       if char != "I":
         stats['overall'][-1] += 1
 
       counter -= 1
 
+  for key in stats:
+    stats[key] = [stats[key][i] / float(groupSize) for i in xrange(len(stats[key]))]
+
   x = range(len(stats['overall']))
 
   yTop = 1.5*max(stats['overall'])
   # general graph
+  # gs = gridspec.GridSpec(2, 2, width_ratios=[3, 1]) 
   a = plt.subplot(221)
 
   plt.plot(x, stats['overall'])
   plt.fill_between(x, stats['overall'],0,color='0.2')
-  plt.axis([0, 1.2*len(x), 0, yTop])
+  plt.axis([0, 1.1*len(x), 0, yTop])
   plt.title('Overall Bus Traffic')
 
   # graph with busRd
@@ -55,7 +59,7 @@ def main():
 
   plt.plot(x, stats['busRd'])
   plt.fill_between(x,stats['busRd'],0,color='0.2')
-  plt.axis([0, 1.2*len(x), 0, yTop])
+  plt.axis([0, 1.1*len(x), 0, yTop])
   plt.title('Bus Traffic from BusRd')
 
   # graph with busRdX
@@ -63,7 +67,7 @@ def main():
 
   plt.plot(x, stats['busRdX'])
   plt.fill_between(x,stats['busRdX'],0,color='0.2')
-  plt.axis([0, 1.2*len(x), 0, yTop])
+  plt.axis([0, 1.1*len(x), 0, yTop])
   plt.title('Bus Traffic from BusRdX')
 
   # graph with busWr
@@ -71,9 +75,12 @@ def main():
 
   plt.plot(x, stats['busWr'])
   plt.fill_between(x,stats['busWr'],0,color='0.2')
-  plt.axis([0, 1.2*len(x), 0, yTop])
-  plt.title('Bus Traffic from BusWr')
+  plt.axis([0, 1.1*len(x), 0, yTop])
+  plt.title('Bus Traffic from BusWr and BusUpg')
 
+  plt.suptitle("Bus Traffic under %s protocol" % outfile,y=1.08, fontsize=20)
+
+  plt.subplots_adjust(hspace=.3)
 #  # graph with busUpg
 #  e = plt.subplot(515)
 #
